39f7236691872ab54196fc832dd79bda39ba53ed
iisafaa <safaahas23@gmail.com>
2023-09-17 23:52:07 +0300

    GB

---
+++ tamadon/Project/script.js
@@ -0,0 +1,4 @@
+// After the animation completes, redirect to the home page
+setTimeout(function() {
+    window.location.href = "staticsPage.html";
+  }, 3000); // Adjust the delay (in milliseconds) as needed
