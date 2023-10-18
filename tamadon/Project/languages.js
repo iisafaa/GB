3ef5cdf09f677f445346f4b633a8fd4f5e9829ea
tallahbunjabi <127039971+tallahbunjabi@users.noreply.github.com>
2023-10-18 16:42:42 +0300

    dddd

--- tamadon/Project/languages.js
+++ tamadon/Project/languages.js
@@ -1,6 +1,5 @@
 // Function to change the language
 function changeLanguage(language) {
-  // Load the appropriate language file based on 'language'
   fetch(`lang-${language}.json`)
     .then((response) => response.json())
     .then((translations) => {
@@ -12,10 +11,6 @@ function changeLanguage(language) {
       });
     });
 
-  // Save the selected language in local storage
-  localStorage.setItem('selectedLanguage', language);
-
-  // Change the stylesheet based on the selected language
   const arabicStylesheet = document.getElementById('ar-stylesheet');
   const englishStylesheet = document.getElementById('eng-stylesheet');
 
@@ -32,23 +27,57 @@ function changeLanguage(language) {
 }
 
 
+document.getElementById('ar').addEventListener('click', () => {
+  changeLanguage('arabic');
+  window.location.href = 'staticsPage.html'; 
+});
 
+document.getElementById('eng').addEventListener('click', () => {
+  changeLanguage('english');
+  window.location.href = 'staticsPage.html'; 
+});
+
+function changeLanguage(language) {
+  fetch(`lang-${language}.json`)
+    .then((response) => response.json())
+    .then((translations) => {
+      document.querySelectorAll('[data-lang]').forEach((element) => {
+        const translationKey = element.getAttribute('data-lang');
+        if (translations[translationKey]) {
+          element.textContent = translations[translationKey];
+        }
+      });
+    });
+
+  localStorage.setItem('selectedLanguage', language);
+
+  const arabicStylesheet = document.getElementById('ar-stylesheet');
+  const englishStylesheet = document.getElementById('eng-stylesheet');
+
+  if (language === 'arabic') {
+    arabicStylesheet.disabled = false;
+    englishStylesheet.disabled = true;
+  } else {
+    arabicStylesheet.disabled = true;
+    englishStylesheet.disabled = false;
+  }
+
+  if (language === 'english') {
+    const buttons = document.querySelectorAll('.Side-button1, .Side-button2');
+    buttons.forEach((button) => {
+      const icon = button.querySelector('.chart') || button.querySelector('.element-plus');
+      const text = button.querySelector('.text-wrapper-12') || button.querySelector('.text-wrapper-13');
+      
+      if (icon && text) {
+        button.appendChild(icon);
+        button.appendChild(text);
+      }
+    });
+  }
+}
 
 // Load language selection on page load
 const storedLanguage = localStorage.getItem('selectedLanguage');
 if (storedLanguage) {
   changeLanguage(storedLanguage);
 }
-
-// Example usage when the Arabic button is clicked
-document.getElementById('ar').addEventListener('click', () => {
-  changeLanguage('arabic');
-  window.location.href = 'staticsPage.html'; // Optionally, navigate to another page
-});
-
-// Example usage when the English button is clicked
-document.getElementById('eng').addEventListener('click', () => {
-  changeLanguage('english');
-  window.location.href = 'staticsPage.html'; // Optionally, navigate to another page
-});
-
